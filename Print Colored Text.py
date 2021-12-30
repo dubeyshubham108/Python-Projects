78a2564e3ae7bd1ef398b1e3c2714563fcb78a00
Shubham Dubey <dubeyshubham154@gmail.com>
2021-12-30 09:57:41 +0530

    Add files via upload
    Printing coloured text in python

---
+++ Print Colored Text.py
@@ -0,0 +1,11 @@
+# Importing Colorama module in python
+import colorama
+# Importing Fore, Back, Style
+from colorama import Fore, Back, Style
+# Here Fore is used for the text color, Back is used for Background color and Style is used for style of the text.
+colorama.init(autoreset=True)
+# Using the above three command printing the coloured text.
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Shubham Dubey "+ Fore.YELLOW+ Back.GREEN+"I am learning Machine Learning")
+print(Back.CYAN+"I love coding ")
+print(Fore.YELLOW + Back.BLUE+ "Coding is now my new hoppy.")
+
